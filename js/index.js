2a326fc6295532f310f8509706f61be8952038ad
mpoxberry <tuc19266@temple.edu>
2016-07-28 13:06:37 -0400

    added js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
 
-    $(".git-rotate").hover(function () {
-      $(this).css({'transform': 'rotate(90deg)'}).css({'transition': '10s'});
+    $(".git-rotate").hover
+    (
+      //function () { $(this).css({'color': 'red'}).css({'transition': '0s'});},
+      function () {
+        $(this).css({'color': 'red'});
+         $(this).css({'transform': 'rotate(90deg)'}).css({'transition': '1s'});
+       },
 
-    })
+      function () {  $(this).css({'color': 'black'}).css({'transform': 'none'}).css({'transition': '1s'});}
+    )
 
 });
