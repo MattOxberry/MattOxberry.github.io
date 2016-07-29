699a9eaa815a755b5402a56a6dff0f443d8d654b
mpoxberry <tuc19266@temple.edu>
2016-07-29 10:31:33 -0400

    java script for icons

--- js/index.js
+++ js/index.js
@@ -4,11 +4,19 @@ $(document).ready(function () {
     (
       //function () { $(this).css({'color': 'red'}).css({'transition': '0s'});},
       function () {
-        $(this).css({'color': 'red'});
-         $(this).css({'transform': 'rotate(90deg)'}).css({'transition': '1s'});
+        $(this).css({'color': '#4078c0'});
+         $(this).css({'transform': 'rotate(45deg)'}).css({'transition': '1s'});
        },
 
       function () {  $(this).css({'color': 'black'}).css({'transform': 'none'}).css({'transition': '1s'});}
     )
 
+    $(".linkin-rotate").hover
+    (
+      function(){
+        $(this).css({'color': '#0077B5'});
+        $(this).css({'transform': 'rotate(45deg)'}).css({'transition': '1s'});
+      },
+      function(){$(this).css({'color': 'black'}).css({'transform': 'none'}).css({'transition': '1s'});}
+    )
 });
