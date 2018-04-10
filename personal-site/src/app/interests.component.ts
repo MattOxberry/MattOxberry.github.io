71ef8172b379bed23e87e23e68d34701aa70630e
Matthew Oxberry <matthew.oxberry@qvc.com>
2018-04-10 09:15:53 -0400

    refactoring project

--- personal-site/src/app/interests.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'interests',
-    templateUrl: './interests.component.html',
-})
-export class InterestsComponent { 
-    title: 'Testing 1 2 3. I like to longboard.';
-}
--- personal-site/src/app/nav-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-nav',
-    templateUrl: './nav-bar.component.html'
-})
-export class NavBarComponent {
-    title = 'app-nav';
-}
