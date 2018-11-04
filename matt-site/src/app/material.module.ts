b11c425a3dbbad6fe7525783e3ad9f6dffab9fde
MattOxberry <tuc19266@temple.edu>
2018-11-03 21:02:26 -0400

    flex-layout works again

--- matt-site/src/app/material.module.ts
+++ matt-site/src/app/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
     MatCardModule
   ],
   exports: [
+    FlexLayoutModule,
     MatCardModule
   ],
   declarations: []
